d8b3c097d0ed234b87bc3c44d5c36bd1cb3aed54
mario mora <jorgemariomoramolina@gmail.com>
2025-01-02 21:14:23 -0500

    validating all inputs on sign up

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,30 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', body('email').isEmail().withMessage('please insert validate information'), authController.postSignup);
+router.post('/signup', [
+        body('email')
+        .isEmail()
+        .withMessage('please insert validate information')
+        .custom((value, { req }) => {
+            if (value === 'mario@mora.com') {
+                throw new Error('forbbiden user');
+            }
+            return true;
+
+
+        }),
+        body('password', 'please enter 4 numbers or letter at least on this password input')
+        .isLength({ min: 4 })
+        .isAlphanumeric(),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('passwords have to match!!');
+            }
+            return true;
+        })
+    ],
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
