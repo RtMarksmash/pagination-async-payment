da82f2e3481f16a534d4cf99a4c9e8a66959fbbf
mario mora <jorgemariomoramolina@gmail.com>
2025-01-05 22:07:13 -0500

    validating to products

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,12 @@ exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: ''
+        },
+        validationErrors: []
     });
 };
 
@@ -28,18 +33,54 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        validationErrors: []
     });
 };
 
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password
+            },
+            validationErrors: errors.array()
+        });
+
+    };
+
+
+
+
     User.findOne({ email: email })
         .then(user => {
             if (!user) {
-                req.flash('error', 'Invalid email or password.');
-                return res.redirect('/login');
+                return res.status(422).render('auth/login', {
+                    path: '/login',
+                    pageTitle: 'Login',
+                    errorMessage: 'invalid password or email',
+                    oldInput: {
+                        email: email,
+                        password: password
+                    },
+                    validationErrors: []
+                });
+
+
             }
             bcrypt
                 .compare(password, user.password)
@@ -73,39 +114,33 @@ exports.postSignup = (req, res, next) => {
         return res.status(422).render('auth/signup', {
             path: '/signup',
             pageTitle: 'Signup',
-            errorMessage: errors.array()[0].msg
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password,
+                confirmPassword: confirmPassword
+            },
+            validationErrors: errors.array()
         });
     }
-    User.findOne({ email: email })
-        .then(userDoc => {
-            if (userDoc) {
-                req.flash(
-                    'error',
-                    'E-Mail exists already, please pick a different one.'
-                );
-                return res.redirect('/signup');
-            }
-            return bcrypt
-                .hash(password, 12)
-                .then(hashedPassword => {
-                    const user = new User({
-                        email: email,
-                        password: hashedPassword,
-                        cart: { items: [] }
-                    });
-                    return user.save();
-                })
-                .then(result => {
-                    res.redirect('/login');
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+    bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+            const user = new User({
+                email: email,
+                password: hashedPassword,
+                cart: { items: [] }
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/login');
         })
         .catch(err => {
             console.log(err);
         });
-};
+}
+
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { body } = require('express-validator');
+const User = require('../models/user');
 
 const authController = require('../controllers/auth');
 
@@ -9,29 +10,46 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login', [
+        body('email')
+        .isEmail()
+        .withMessage('please insert validate information')
+        .normalizeEmail(),
+        body('password', 'this password need more letters at least 4')
+        .isLength({ min: 4 })
+        .isAlphanumeric()
+        .trim()
+    ],
+    authController.postLogin);
 
 router.post('/signup', [
         body('email')
         .isEmail()
-        .withMessage('please insert validate information')
+        .withMessage('please insert validate 2 information')
         .custom((value, { req }) => {
-            if (value === 'mario@mora.com') {
-                throw new Error('forbbiden user');
-            }
-            return true;
-
-
-        }),
+            /*  if (value === 'mario@mora.com') {
+                 throw new Error('forbbiden user');
+             }
+             return true; */
+            return User.findOne({ email: value }).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('email already exist, please pick a diferent email')
+                }
+            })
+        })
+        .normalizeEmail(),
         body('password', 'please enter 4 numbers or letter at least on this password input')
         .isLength({ min: 4 })
-        .isAlphanumeric(),
-        body('confirmPassword').custom((value, { req }) => {
+        .isAlphanumeric()
+        .trim(),
+        body('confirmPassword')
+        .custom((value, { req }) => {
             if (value !== req.body.password) {
                 throw new Error('passwords have to match!!');
             }
             return true;
         })
+        .trim()
     ],
     authController.postSignup
 );
