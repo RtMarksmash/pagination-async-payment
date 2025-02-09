b41f68ada664f5de5651eb3a784f7be9772c43d8
mario mora <jorgemariomoramolina@gmail.com>
2025-02-09 15:07:01 -0500

    adapting stripe platform

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,7 @@ const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
 const fileHelper = require('../util/file');
+const stripe = require('stripe')('sk_test_51QoVsMJWV4UeW9AnXmjmR4rTvhWriYat9dl4IxdCSkw1MrekpLtk2Emgoo2wPgtjtVlWWVQYFKTvyhWlLVg2MLDW00B4wh8pCa')
 
 const PdfDocument = require('pdfkit');
 
@@ -135,22 +136,75 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
+    let products;
+    let total = 0;
     req.user
         .populate('cart.items.productId')
         .then(user => {
-            const products = user.cart.items;
-            let total = 0;
+            products = user.cart.items;
+            total = 0;
             products.forEach(p => {
                 total += p.quantity * p.productId.price;
-            })
-            res.render('shop/checkout', {
-                path: '/checkout',
-                pageTitle: 'payment',
-                products: products,
-                totalSum: total
+            });
+
+            return stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                line_items: products.map(p => {
+                    return {
+                        name: p.productId.title,
+                        description: p.productId.description,
+                        amount: p.productId.price * 100,
+                        currency: 'usd',
+                        quantity: p.quantity
+                    };
+                }),
+                success_url: req.protocol + '://' + req.get('host') + '/checkout/success', // => http://localhost:3000
+                cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
             });
         })
-        .catch(err => console.log(err));
+        .then(session => {
+            res.render('shop/checkout', {
+                path: '/checkout',
+                pageTitle: 'Checkout',
+                products: products,
+                totalSum: total,
+                sessionId: session.id
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+    req.user
+        .populate('cart.items.productId')
+        .then(user => {
+            const products = user.cart.items.map(i => {
+                return { quantity: i.quantity, product: {...i.productId._doc } };
+            });
+            const order = new Order({
+                user: {
+                    email: req.user.email,
+                    userId: req.user
+                },
+                products: products
+            });
+            return order.save();
+        })
+        .then(result => {
+            return req.user.clearCart();
+        })
+        .then(() => {
+            res.redirect('/orders');
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
 
 
